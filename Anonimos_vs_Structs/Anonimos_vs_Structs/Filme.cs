818938282457aab88988cc06d7bb580a75afaea8
Nicolas de Campos Fischer <79231180+NicolasFischer2002@users.noreply.github.com>
2023-12-16 18:05:06 -0300

    Add files via upload

---
+++ Anonimos_vs_Structs/Anonimos_vs_Structs/Filme.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.Data.SqlTypes;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonimos_vs_Structs
+{
+    // <===== Construtor da Classe =====> //
+    public class Filme(long id, string titulo, DateTime dataLancamento, string diretor, string genero, float valor)
+    {
+        public long Id { get; init; } = id;
+        public string Titulo { get; init; } = titulo;
+        public DateTime dataLancamento { get; init; } = dataLancamento;
+        public string Diretor { get; init; } = diretor;
+        public string Genero { get; init; } = genero;
+        public float Valor { get; set; } = valor;
+
+        private struct FilmesMaisBaratos
+        {
+            public string Titulo;
+            public float Valor;
+        }
+
+        private struct TituloDiretorFilme
+        {
+            public string Titulo;
+            public string Diretor;
+        }
+
+        public static Filme[] FilmesCadastrados()
+        {
+            Filme[] filmes =
+            [
+                 new (1, "Um Sonho de Liberdade", new DateTime(1994, 9, 23), "Frank Darabont", "Drama", 50.00f),
+                 new (2, "Enigma do Outro Mundo", new DateTime(1982, 6, 25), "John Carpenter", "Ficção Científica", 10.00f),
+                 new (3, "O Resgate do Soldado Ryan", new DateTime(1998, 7, 24), "Steven Spielberg", "Drama de Guerra",25.00f),
+                 new (4, "O Poderoso Chefão", new DateTime(1972, 3, 24), "Francis Ford Coppola", "Crime, Drama", 10.00f),
+                 new (5, "Predador", new DateTime(1987, 6, 12), "John McTiernan", "Ação, Ficção Científica", 15.00f),
+                 new (6, "Logan", new DateTime(2017, 3, 3), "James Mangold", "Ação, Drama, Ficção Científica", 10.00f),
+                 new (7, "A Ilha do Medo", new DateTime(2010, 2, 19), "Martin Scorsese", "Mistério, Suspense", 15.00f),
+                 new (8, "Forrest Gump", new DateTime(1994, 7, 6), "Robert Zemeckis", "Comédia, Drama, Romance", 20.00f),
+                 new (9, "Pulp Fiction", new DateTime(1994, 10, 14), "Quentin Tarantino", "Crime, Drama", 12.00f),
+                 new (10, "Dia de Treinamento", new DateTime(2001, 10, 5), "Antoine Fuqua", "Crime, Drama, Thriller", 12.50f),
+                 new (11, "Seven", new DateTime(1995, 9, 22), "David Fincher", "Crime, Drama, Mistério", 22.50f),
+                 new (13, "Exterminador do Futuro 2", new DateTime(1991, 7, 3), "James Cameron", "Ação, Ficção Científica", 25.50f),
+                 new (14, "Indiana Jones e os Caçadores da Arca Perdida", new DateTime(1981, 6, 12), "Steven Spielberg", "Ação, Aventura", 30.00f),
+                 new (15, "O Show de Truman", new DateTime(1998, 6, 5), "Peter Weir", "Comédia, Drama, Ficção Científica", 12.50f),
+                 new (16, "O Senhor dos Anéis: As Duas Torres", new DateTime(2002, 12, 18), "Peter Jackson", "Ação, Aventura, Drama", 50.00f),
+                 new (17, "O Décimo Terceiro Guerreiro", new DateTime(1999, 8, 27), "John McTiernan, Michael Crichton", "Ação, Aventura, História", 15.00f),
+                 new (18, "Rambo: Programado Para Matar", new DateTime(1982, 10, 22), "Ted Kotcheff", "Ação, Aventura, Drama", 30.00f),
+                 new (19, "Alien: O Oitavo Passageiro", new DateTime(1979, 5, 25), "Ridley Scott", "Horror, Sci-Fi", 15.50f),
+                 new (20, "Toy Story", new DateTime(1995, 11, 22), "John Lasseter", "Animação, Aventura, Comédia", 10.00f)
+            ];
+            return filmes;
+        }
+
+        public static void ExibeFilmesCadastrados(Filme[] filmes)
+        {
+            Console.WriteLine("<===== Todos os filmes cadastrados =====>");
+            foreach (Filme filme in filmes)
+                Console.WriteLine($"{filme.Titulo}");
+        }
+
+        public static void ExibeFilmesMaisCaros(Filme[] filmes)
+        {
+            // Exemplo prático de classe anônima, uma List<anônima>
+            var filmesMaisCaros = filmes
+                .OrderByDescending(f => f.Valor)
+                .Where(f => f.Valor == filmes.Max(x => x.Valor))
+                .Select(f => new { f.Titulo, f.Valor })
+                .ToList();
+
+            Console.WriteLine("<===== Filmes mais caros =====>");
+            foreach (var filme in filmesMaisCaros)
+                Console.WriteLine($"{filme.Titulo} - {filme.Valor.ToString("C", CultureInfo.GetCultureInfo("pt-BR"))}");
+
+            // Como filmesMaisCaros é uma lista de objetos anônimos, não é possível alterar o tipo da variável filme no foreach
+            // para um tipo conhecido, pois ele também é um tipo anônimo.
+            // Tipos anônimos são úteis para quando se precisa de um tipo de objeto ainda não mapeado e que não vai possuir vida longa
+            // dentro do programa. Utilizar classes anônimas agiliza o desenvolvimento e gera menos código, no entando o código gerado pode ser menos legível,
+            // aliado à um consumo maior de memória e de um desempenho inferior, já que por ser uma classe, o objeto será alocado no Heap da memória.
+        }
+
+        public static void ExibeFilmesMaisBaratos(Filme[] filmes)
+        {
+            // Abordagem utilizando Structs
+            IReadOnlyList<FilmesMaisBaratos> filmesMaisBaratos = filmes
+                .OrderBy(f => f.Valor)
+                .Where(f => f.Valor == filmes.Min(x => x.Valor))
+                .Select(f => new FilmesMaisBaratos { Titulo = f.Titulo, Valor = f.Valor })
+                .ToList();
+
+            Console.WriteLine("<===== Filmes mais baratos =====>");
+            foreach (FilmesMaisBaratos filme in filmesMaisBaratos)
+                Console.WriteLine($"{filme.Titulo} - {filme.Valor.ToString("C", CultureInfo.GetCultureInfo("pt-BR"))}");
+
+            // Diferente das classe anônimas, o código aqui é mais legível, pois no foreach é possível determinar o tipo da variável filme,
+            // que é uma Struct definida. Características como legibilidade, reaproveitamento de código e eficiência acompanham as Structs,
+            // visto que é possível identificar os tipos e melhorar a semântica do código, pode-se utilizar as Structs à vontade uma vez que definidas,
+            // e as structs compactas são armazenadas na Stack ao invés de irem para o Heap, facilitando a localização dos valores contidos na variável.
+            // Em contra partida, utilizar Structs é mais trabalhoso e gera mais código.
+        }
+
+        public static void ExibeValorMedioDosFilmes(Filme[] filmes)
+        {
+            float valorMedio = filmes.Average(f => f.Valor);
+
+            Console.WriteLine("<===== Valor médio dos filmes =====>");
+            Console.WriteLine(valorMedio);
+        }
+
+        public static void ExibeFilmesOrdPorPreco(Filme[] filmes)
+        {
+            IReadOnlyDictionary<string, float> filmesOrdPorPreco = filmes
+                .OrderByDescending(f => f.Valor)
+                .ToDictionary(f => f.Titulo, f => f.Valor);
+
+            Console.WriteLine("<===== Filmes ordenados por preço =====>");
+            foreach (var filme in filmesOrdPorPreco)
+                Console.WriteLine($"{filme.Key}, {filme.Value.ToString("C", CultureInfo.GetCultureInfo("pt-BR"))}");
+        }
+
+        public static void FilmesOrdPorLancamento(Filme[] filmes)
+        {
+            // Mais um exemplo de classe anônima
+            var filmesOrdPorDataLancamento = filmes.OrderByDescending(f => f.dataLancamento).Select(f => new { f.Titulo, f.dataLancamento });
+            
+            Console.WriteLine("<===== Filmes ordenados por data de lançamento =====>");
+            foreach (var filme in filmesOrdPorDataLancamento)
+                Console.WriteLine($"{filme.Titulo}, {filme.dataLancamento.ToString("dd/MM/yyyy")}");
+        }
+
+        public static void ExibeFilmesOrdPorID(Filme[] filmes)
+        {
+            IReadOnlyDictionary<long, string> filmesOrdPorID = filmes
+                .OrderBy(f => f.Id)
+                .ToDictionary(f => f.Id, f => f.Titulo);
+
+            Console.WriteLine("<===== Filmes ordenados por ID =====>");
+            foreach (var filme in filmesOrdPorID)
+                Console.WriteLine($"{filme.Key}, {filme.Value}");
+        }
+
+        public static void ExibeIdTituloDiretorFilme(Filme[] filmes)
+        {
+            // Um dicionário de Structs
+            IReadOnlyDictionary<long, TituloDiretorFilme> IdTituloDiretorFilme = filmes
+                .OrderBy(f => f.Id)
+                .ToDictionary(f => f.Id, f => new TituloDiretorFilme { Titulo = f.Titulo, Diretor = f.Diretor });
+
+            Console.WriteLine("<===== Filmes e seus diretores, ordenados por ID =====>");
+            foreach (var filme in IdTituloDiretorFilme)
+                Console.WriteLine($"{filme.Key} - {filme.Value.Titulo}, {filme.Value.Diretor}");
+        }
+    }
+}
---
+++ Anonimos_vs_Structs/Anonimos_vs_Structs/Program.cs
@@ -0,0 +1,87 @@
+﻿
+
+using Anonimos_vs_Structs;
+using System.Linq.Expressions;
+
+try
+{
+    Filme[] filmesCadastrados = Filme.FilmesCadastrados();
+    do
+    {
+        Console.WriteLine("<===== Relatórios =====>");
+        Console.WriteLine("\n - Digite 1 para vizualizar todos os filmes cadastrados");
+        Console.WriteLine(" - Digite 2 para vizualizar o filme mais caro");
+        Console.WriteLine(" - Digite 3 para vizualizar o filme mais barato");
+        Console.WriteLine(" - Digite 4 para vizualizar a média de preço dos filmes");
+        Console.WriteLine(" - Digite 5 para vizualizar os filmes ordenados por preço");
+        Console.WriteLine(" - Digite 6 para vizualizar os filmes ordenados por data de lançamento");
+        Console.WriteLine(" - Digite 7 para vizualizar código e nome do filme");
+        Console.WriteLine(" - Digite 8 para vizualizar o código, titulo e o diretor do filme");
+        Console.WriteLine(" - Digite 0 para sair do programa");
+
+        Console.Write("\n Digite o número correspondente à opção desejada: ");
+        string? codSelecionado = Console.ReadLine();
+        if (!string.IsNullOrEmpty(codSelecionado))
+        {
+            switch (codSelecionado)
+            {
+                case "1":
+                    Console.Clear();
+                    Filme.ExibeFilmesCadastrados(filmesCadastrados);
+                    break;
+                
+                case "2":
+                    Console.Clear();
+                    Filme.ExibeFilmesMaisCaros(filmesCadastrados);
+                    break;
+
+                case "3":
+                    Console.Clear();
+                    Filme.ExibeFilmesMaisBaratos(filmesCadastrados);
+                    break;
+
+                case "4":
+                    Console.Clear();
+                    Filme.ExibeValorMedioDosFilmes(filmesCadastrados);
+                    break;
+                case "5":
+                    Console.Clear();
+                    Filme.ExibeFilmesOrdPorPreco(filmesCadastrados);
+                    break;
+
+                case "6":
+                    Console.Clear();
+                    Filme.FilmesOrdPorLancamento(filmesCadastrados);
+                    break;
+                
+                case "7":
+                    Console.Clear();
+                    Filme.ExibeFilmesOrdPorID(filmesCadastrados);
+                    break;
+
+                case "8":
+                    Console.Clear();
+                    Filme.ExibeIdTituloDiretorFilme(filmesCadastrados);
+                    break;
+
+                case "0":
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.Clear();
+                    Console.WriteLine("\nOpção inválida! Por favor, escolha um número válido!");
+                    break;
+            }
+            Console.WriteLine("\nPrecione enter para obter outro relatório...");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+    } while (true);
+
+}
+catch (Exception e)
+{
+    throw new Exception("Erro ao trabalhar com Classe Anônima e Struct! " + e.Message);
+}
